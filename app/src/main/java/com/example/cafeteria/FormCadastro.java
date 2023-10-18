57f14170b286a1d9c5857228b0234add0218f699
Weuller Oliveira <weullerwww@gmail.com>
2023-10-18 16:55:37 -0300

    Conectando tela Login com Cadastro

---
+++ app/src/main/java/com/example/cafeteria/FormCadastro.java
@@ -0,0 +1,16 @@
+package com.example.cafeteria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+    }
+
+}
--- app/src/main/java/com/example/cafeteria/FormLogin.java
+++ app/src/main/java/com/example/cafeteria/FormLogin.java
@@ -2,9 +2,14 @@ package com.example.cafeteria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,21 @@ public class FormLogin extends AppCompatActivity {
         setContentView(R.layout.activity_form_login);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(FormLogin.this, FormCadastro.class);
+                startActivity(intent);
+            }
+
+        });
     }
+
+    private void IniciarComponentes() {
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+    }
+
 }
