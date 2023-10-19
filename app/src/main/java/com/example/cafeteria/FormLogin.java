873a7b6e603301ee8ee5485240ece4f7fb46ea24
Weuller Oliveira <weullerwww@gmail.com>
2023-10-19 10:27:01 -0300

    Autenticacao de login

--- app/src/main/java/com/example/cafeteria/FormLogin.java
+++ app/src/main/java/com/example/cafeteria/FormLogin.java
@@ -1,15 +1,31 @@
 package com.example.cafeteria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FormLogin extends AppCompatActivity {
 
     private TextView text_tela_cadastro;
+    private EditText edit_email, edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressBar;
+    private String[] mensagens = {"Preencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +33,7 @@ public class FormLogin extends AppCompatActivity {
         setContentView(R.layout.activity_form_login);
 
         getSupportActionBar().hide();
-        IniciarComponentes();
+        iniciarComponentes();
 
         text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,10 +44,62 @@ public class FormLogin extends AppCompatActivity {
             }
 
         });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()) {
+                    mostrarMensagem(v, mensagens[0]);
+                } else {
+                    autenticarUsuario();
+                }
+            }
+        });
     }
 
-    private void IniciarComponentes() {
+    private void autenticarUsuario() {
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            telaMinhaConta();
+                        }
+                    }, 3000);
+                }
+            }
+        });
+    }
+
+    private void telaMinhaConta() {
+        Intent intent = new Intent(FormLogin.this, MinhaConta.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void iniciarComponentes() {
         text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        progressBar = findViewById(R.id.progressbar);
     }
 
+    private void mostrarMensagem(View v, String msg) {
+        Snackbar snackbar = Snackbar.make(v, msg, Snackbar.LENGTH_SHORT);
+        snackbar.setBackgroundTint(Color.WHITE);
+        snackbar.setTextColor(Color.BLACK);
+        snackbar.show();
+    }
 }
